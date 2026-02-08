eabeadd913227ebc0b8e0b9bc124b9e241560bb9
GuilhermeMth <guilhermemfranca06@gmail.com>
2026-02-08 07:57:52 -0300

    feat: implement OngsBankAccount module with CRUD operations and DTOs

--- src/modules/ong-profile/ong-profiles.module.ts
+++ src/modules/ong-profile/ong-profiles.module.ts
@@ -3,7 +3,7 @@ import { OngProfilesService } from './ong-profiles.service';
 import { OngProfilesController } from './ong-profiles.controller';
 import { PrismaModule } from '../../prisma/prisma.module';
 import { ImageProcessingService } from '../../common/services/image-processing.service';
-import { OngsBankAccountModule } from 'src/ongs-bank-account/ongs-bank-account.module';
+import { OngsBankAccountModule } from 'src/modules/ongs-bank-account/ongs-bank-account.module';
 
 @Module({
   imports: [PrismaModule, OngsBankAccountModule],
